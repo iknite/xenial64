8e5c34517cf81bb08414f0abbb57a60f3b8d8434
iknite <enrique@iknite.com>
2016-05-17 20:00:32 +0200

    Fixes to make it work with xenial

--- src/provision/update.sh
+++ src/provision/update.sh
@@ -7,6 +7,4 @@ chown root:root /etc/apt/apt.conf.d/docker-*
 chmod 644 /etc/apt/apt.conf.d/docker-*
 
 # make sure we're fully up-to-date
-apt-get update && apt-get dist-upgrade -y
-
-reboot
+apt-get update && apt-get upgrade -y
