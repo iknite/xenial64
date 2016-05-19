e0d4f525857b42907e437c8fff117311df816b3a
iknite <enrique@iknite.com>
2016-05-19 15:50:46 +0200

    configure dist-upgrade and reboot correctly

--- src/provision/update.sh
+++ src/provision/update.sh
@@ -7,4 +7,12 @@ chown root:root /etc/apt/apt.conf.d/docker-*
 chmod 644 /etc/apt/apt.conf.d/docker-*
 
 # make sure we're fully up-to-date
-apt-get update && apt-get upgrade -y
+apt-get update && apt-get dist-upgrade -y
+
+# disable all active netowrk interfaces to allow packer to continue with next provisioner
+for net in $(ip -brief link | awk '/UP / {print $1}' ); do
+    ifdown $net
+done
+
+reboot
+sleep 60
